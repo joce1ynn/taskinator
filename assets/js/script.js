d4da5ea292a7aed5312ced29662f4838b858f9d7
Jocelyn Tian <jocelyntianww@gmail.com>
2021-11-09 16:53:56 -0700

    add ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,42 @@
-r
+// console.dir(window.document);
+// displays the HTML element as an object, a DOM element
+
+// var buttonEl = document.querySelector("#save-task");
+// console.log(buttonEl);
+
+// buttonEl.addEventListener("click", function(){
+//     alert("button clicked");
+// });
+
+// document.createElement("li");
+// 创建一个 DOM 元素对象
+
+// var taskItemEl = document.createElement("li");
+// taskItemEl.textContent = "hello";
+// taskItemEl;
+
+// var taskToDoEl = document.querySelector("#tasks-to-do");
+// taskToDoEl;
+
+// taskToDoEl.appendChild(taskItemEl);
+// taskItemEl.className = "task-item"; // assign same class name;
+
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+// buttonEl.addEventListener("click", function() {
+//   var listItemEl = document.createElement("li");
+//   listItemEl.className = "task-item";
+//   listItemEl.textContent = "This is a new task.";
+//   tasksToDoEl.appendChild(listItemEl);
+// });
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+  };
+
+  buttonEl.addEventListener("click", createTaskHandler);
+
