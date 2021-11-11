68e3f082b178910c731c0682a2cea0d1f3510658
Jocelyn Tian <jocelyntianww@gmail.com>
2021-11-11 16:33:23 -0700

    handle form

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,8 +21,9 @@
 // taskToDoEl.appendChild(taskItemEl);
 // taskItemEl.className = "task-item"; // assign same class name;
 
-var buttonEl = document.querySelector("#save-task");
+// var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
 
 // buttonEl.addEventListener("click", function() {
 //   var listItemEl = document.createElement("li");
@@ -31,12 +32,17 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 //   tasksToDoEl.appendChild(listItemEl);
 // });
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  };
+var createTaskHandler = function (event) {
+  event.preventDefault();
 
-  buttonEl.addEventListener("click", createTaskHandler);
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
 
+};
+
+// buttonEl.addEventListener("click", createTaskHandler);
+// 因为我们的目标是整个form而不是button，所以我们不能再使用click event listener
+
+formEl.addEventListener("submit", createTaskHandler);
