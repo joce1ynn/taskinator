7e5a739ec988af038752bb33cf181273845ed81f
Jocelyn Tian <jocelyntianww@gmail.com>
2021-11-11 18:15:34 -0700

    add ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,8 +22,6 @@
 // taskItemEl.className = "task-item"; // assign same class name;
 
 // var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
-var formEl = document.querySelector("#task-form");
 
 // buttonEl.addEventListener("click", function() {
 //   var listItemEl = document.createElement("li");
@@ -32,29 +30,79 @@ var formEl = document.querySelector("#task-form");
 //   tasksToDoEl.appendChild(listItemEl);
 // });
 
-var createTaskHandler = function (event) {
+// var createTaskHandler = function (event) {
+//   event.preventDefault();
+
+//   var taskNameInput = document.querySelector("input[name='task-name']").value;
+//   // console.dir(taskNameInput);
+//   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+//   // .value只选择他的值
+
+//   // create list item
+//   var listItemEl = document.createElement("li");
+//   listItemEl.className = "task-item";
+
+//   // create div to hold task info and add to list item
+//   var taskInfoEl = document.createElement("div");
+//   // give it a class name
+//   taskInfoEl.className = "task-info";
+
+//   // add HTML content to div
+//   taskInfoEl.innerHTML =
+//     '<h3 class="task-name">' +
+//     taskNameInput +
+//     "</h3><span class='task-type'>" +
+//     taskTypeInput +
+//     "</span>";
+//   listItemEl.appendChild(taskInfoEl);
+
+//   // add entire list item to list
+//   tasksToDoEl.appendChild(listItemEl);
+// };
+
+// buttonEl.addEventListener("click", createTaskHandler);
+// 因为我们的目标是整个form而不是button，所以我们不能再使用click event listener
+
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var taskFormHandler = function (event) {
   event.preventDefault();
-
   var taskNameInput = document.querySelector("input[name='task-name']").value;
-  // console.dir(taskNameInput);
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
-  // .value只选择他的值
 
+  // check if input values are empty strings
+  // the ! is used to check for the false value.
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+
+  formEl.reset();
+
+  // package up data as an object
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput,
+  };
+
+  // send it as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function (taskDataObj) {
   // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
 
   // create div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
-  // give it a class name
   taskInfoEl.className = "task-info";
-
-  // add HTML content to div
   taskInfoEl.innerHTML =
-    '<h3 class="task-name">' +
-    taskNameInput +
+    "<h3 class='task-name'>" +
+    taskDataObj.name +
     "</h3><span class='task-type'>" +
-    taskTypeInput +
+    taskDataObj.type +
     "</span>";
   listItemEl.appendChild(taskInfoEl);
 
@@ -62,7 +110,4 @@ var createTaskHandler = function (event) {
   tasksToDoEl.appendChild(listItemEl);
 };
 
-// buttonEl.addEventListener("click", createTaskHandler);
-// 因为我们的目标是整个form而不是button，所以我们不能再使用click event listener
-
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
